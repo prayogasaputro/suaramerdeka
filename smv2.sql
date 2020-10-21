d43c6dfc5b9e43792add0512f8f91e2a3881c036
prayogasaputro <64127534+prayogasaputro@users.noreply.github.com>
2020-10-21 23:31:27 +0700

    Update smv2.sql

--- smv2.sql
+++ smv2.sql
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin`
 --
 
-CREATE TABLE `admin` (
+CREATE TABLE "admin" (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -37,7 +37,7 @@ CREATE TABLE `admin` (
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`username`, `password`) VALUES
+INSERT INTO "admin" ("username", "password") VALUES
 ('admin', 'admin');
 
 -- --------------------------------------------------------
@@ -46,7 +46,7 @@ INSERT INTO `admin` (`username`, `password`) VALUES
 -- Table structure for table `iklan`
 --
 
-CREATE TABLE `iklan` (
+CREATE TABLE "iklan" (
   `id` int(255) NOT NULL,
   `fullName` varchar(255) NOT NULL,
   `addressLine1` varchar(255) NOT NULL,
@@ -63,7 +63,7 @@ CREATE TABLE `iklan` (
 -- Dumping data for table `iklan`
 --
 
-INSERT INTO `iklan` (`id`, `fullName`, `addressLine1`, `email`, `iklan`, `tanggal`, `unik`, `status_iklan`, `tgl_invoice`, `bayar`) VALUES
+INSERT INTO "iklan" ("id", "fullName", "addressLine1", "email", "iklan", "tanggal", "unik", "status_iklan", "tgl_invoice", "bayar") VALUES
 (170, 'Dio Alfian', 'Jalan Sisingamangaraja', 'dioalfian31@gmail.com', 'JUAL RUMAH Blok 3, Puri Depok,dekat kmpus UI,Lt.138/Lb.120,SHM,Siap huni,jual cepat!hrg.nego!! Hub.0813 168 405 â€“ 0812 726 510', '2019-07-13', '177iz0z', 'Iklan dapat ditayangkan', '2019-07-04 06:00:48', 55000),
 (171, 'Muhammad Rafli', 'Jalan Timun', 'raflim14@gmail.com', 'DIJUAL CEPAT, rmh 2 lantai di Komp.Villa Asean, Pd. Cabe TangSel,SHM, LT 1.000m2, LB -+500m2, Hub: Dewi: 08129965606', '2019-07-20', '4245luc', 'Iklan dapat ditayangkan', '2019-07-04 06:05:46', 44000),
 (172, 'Rifka Suci', 'Jalan Belimbing', 'sucir18@gmail.com', 'DJUAL RMH BARU dipusat kt Semarang, Lt120/180 KT4,KM2,KP1, 1300/1200W,Grsi,AC ,Dkt TOL.850Jt.BU.Jl.Setia 1 Rt4/8 Jtwrngin. T. 081287053154', '2019-07-10', '742ydpo', 'Iklan dapat ditayangkan', '2019-07-04 06:08:07', 55000),
@@ -80,7 +80,7 @@ INSERT INTO `iklan` (`id`, `fullName`, `addressLine1`, `email`, `iklan`, `tangga
 -- Table structure for table `iklan2`
 --
 
-CREATE TABLE `iklan2` (
+CREATE TABLE "iklan2" (
   `id2` int(255) NOT NULL,
   `iklan2` varchar(320) NOT NULL,
   `tanggal2` date NOT NULL,
@@ -95,7 +95,7 @@ CREATE TABLE `iklan2` (
 -- Dumping data for table `iklan2`
 --
 
-INSERT INTO `iklan2` (`id2`, `iklan2`, `tanggal2`, `status_iklan2`, `userid`, `unik2`, `tgl_invoice2`, `bayar2`) VALUES
+INSERT INTO "iklan2" ("id2", "iklan2", "tanggal2", "status_iklan2", "userid", "unik2", "tgl_invoice2", "bayar2") VALUES
 (54, 'J/K RmhTkt Di Bandung II Rt03/12 Maskumambang,View Bgs,Dkt Bndara, Hrg 700Jt Nego,LT102m2 LB120m2 6KT,1KM,1KP, Artetis,PLN 800W,Grsi.Tlp/WA:085740073021', '2019-07-19', 'Iklan dapat ditayangkan', 3, '568odle', '2019-07-04 06:12:54', 55000),
 (55, 'Dijual Cpt Rumah Murah LB38/LT72.Hrga 150Jt.Nego. 2KT,1KM. Sisa Blkang 4M, Sisa Depan 3M, Air Artetis.Perum Kawali Blok 2 No.5 Buah Batu. Hp/Wa: 082198280001', '2019-07-24', 'Iklan sedang dalam proses pengecekan', 3, '836v8f6', '2019-07-04 06:15:15', 55000),
 (56, 'Jual Rumah HM,Intan Permai Blok D.170 LT=85m2 (9x 10) Fullbang, 2 Lantai, 3KT, 2KM, Hrg 410jt Nego. Hub:08139872640', '2019-07-12', 'Iklan sedang dalam proses pengecekan', 4, '1880l6$', '2019-07-04 06:16:28', 44000),
@@ -117,7 +117,7 @@ INSERT INTO `iklan2` (`id2`, `iklan2`, `tanggal2`, `status_iklan2`, `userid`, `u
 -- Table structure for table `image_tb`
 --
 
-CREATE TABLE `image_tb` (
+CREATE TABLE "image_tb" (
   `imageid` int(255) NOT NULL,
   `img_location` varchar(255) NOT NULL,
   `unik` varchar(255) NOT NULL
@@ -127,7 +127,7 @@ CREATE TABLE `image_tb` (
 -- Dumping data for table `image_tb`
 --
 
-INSERT INTO `image_tb` (`imageid`, `img_location`, `unik`) VALUES
+INSERT INTO "image_tb" ("imageid", "img_location", "unik") VALUES
 (51, 'upload/55000_1562195075.jpg', '177iz0z'),
 (52, 'upload/44000_1562195223.jpg', '4245luc'),
 (53, 'upload/55000_1562195298.jpg', '742ydpo'),
@@ -141,7 +141,7 @@ INSERT INTO `image_tb` (`imageid`, `img_location`, `unik`) VALUES
 -- Table structure for table `image_tb2`
 --
 
-CREATE TABLE `image_tb2` (
+CREATE TABLE "image_tb2" (
   `imageid2` int(255) NOT NULL,
   `img_location2` varchar(255) NOT NULL,
   `unik2` varchar(255) NOT NULL,
@@ -152,7 +152,7 @@ CREATE TABLE `image_tb2` (
 -- Dumping data for table `image_tb2`
 --
 
-INSERT INTO `image_tb2` (`imageid2`, `img_location2`, `unik2`, `userid`) VALUES
+INSERT INTO "image_tb2" ("imageid2", "img_location2", "unik2", "userid") VALUES
 (3, 'upload/55000_1562195597.jpg', '568odle', 3),
 (4, 'upload/55000_1562195736.jpg', '836v8f6', 3),
 (5, 'upload/44000_1562195803.jpg', '1880l6$', 4),
@@ -172,7 +172,7 @@ INSERT INTO `image_tb2` (`imageid2`, `img_location2`, `unik2`, `userid`) VALUES
 -- Table structure for table `user`
 --
 
-CREATE TABLE `user` (
+CREATE TABLE "user" (
   `userid` int(11) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
@@ -184,7 +184,7 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`userid`, `email`, `password`, `code`, `verify`) VALUES
+INSERT INTO "user" ("userid", "email", "password", "code", "verify") VALUES
 (3, 'novitadian13@yahoo.com', '5240c3fea9a2ead528fe3f66f3cde4ca', 'Kes9pay6kQ5i', 1),
 (4, 'diannovita42@yahoo.com', 'd571ba5754c314a2cc215bf99ed76564', '5Gbku8OWrSxE', 1),
 (5, 'danggraini92@yahoo.com', 'd571ba5754c314a2cc215bf99ed76564', '8u3s5gmzDa4i', 1);
@@ -196,37 +196,37 @@ INSERT INTO `user` (`userid`, `email`, `password`, `code`, `verify`) VALUES
 --
 -- Indexes for table `admin`
 --
-ALTER TABLE `admin`
+ALTER TABLE "admin"
   ADD PRIMARY KEY (`username`);
 
 --
 -- Indexes for table `iklan`
 --
-ALTER TABLE `iklan`
+ALTER TABLE "iklan"
   ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `iklan2`
 --
-ALTER TABLE `iklan2`
+ALTER TABLE "iklan2"
   ADD PRIMARY KEY (`id2`);
 
 --
 -- Indexes for table `image_tb`
 --
-ALTER TABLE `image_tb`
+ALTER TABLE "image_tb"
   ADD PRIMARY KEY (`imageid`);
 
 --
 -- Indexes for table `image_tb2`
 --
-ALTER TABLE `image_tb2`
+ALTER TABLE "image_tb2"
   ADD PRIMARY KEY (`imageid2`);
 
 --
 -- Indexes for table `user`
 --
-ALTER TABLE `user`
+ALTER TABLE "user"
   ADD PRIMARY KEY (`userid`);
 
 --
@@ -236,31 +236,31 @@ ALTER TABLE `user`
 --
 -- AUTO_INCREMENT for table `iklan`
 --
-ALTER TABLE `iklan`
+ALTER TABLE "iklan"
   MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=179;
 
 --
 -- AUTO_INCREMENT for table `iklan2`
 --
-ALTER TABLE `iklan2`
+ALTER TABLE "iklan2"
   MODIFY `id2` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
 
 --
 -- AUTO_INCREMENT for table `image_tb`
 --
-ALTER TABLE `image_tb`
+ALTER TABLE "image_tb"
   MODIFY `imageid` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- AUTO_INCREMENT for table `image_tb2`
 --
-ALTER TABLE `image_tb2`
+ALTER TABLE "image_tb2"
   MODIFY `imageid2` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
-ALTER TABLE `user`
+ALTER TABLE "user"
   MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
