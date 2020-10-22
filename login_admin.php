35ded2698dd191226b6a5adc025d772f6b874f01
prayogasaputro <64127534+prayogasaputro@users.noreply.github.com>
2020-10-22 19:55:06 +0700

    Update login_admin.php

--- login_admin.php
+++ login_admin.php
@@ -1,19 +1,19 @@
 <?php
    session_start();
    require_once("koneksi.php");
-   $username = $_POST['username'];
-   $pass = $_POST['password'];
+   $username = FILTER_INPUT(INPUT_POST, 'username');
+   $pass = FILTER_INPUT(INPUT_POST, 'password');
 
    $sql = "SELECT * FROM admin WHERE username = '$username'";
    $query = $db->query($sql);
    $hasil = $query->fetch_assoc();
    if($query->num_rows == 0) {
-     echo "<div align='center'>Username Belum Terdaftar! <a href='admin.php'>coba lagi</a></div>";
+     <?= "<div align='center'>Username Belum Terdaftar! <a href='admin.php'>coba lagi</a></div>">?;
    } else {
      if($pass <> $hasil['password']) {
-       echo "<div align='center'>Password salah! <a href='admin.php'>coba lagi</a></div>";
+       <?= "<div align='center'>Password salah! <a href='admin.php'>coba lagi</a></div>">?;
      } else {
-       $_SESSION['username'] = $hasil['username'];
+       FILTER_INPUT(INPUT_SESSION, 'username') = $hasil['username'];
        header('location:awal_admin.php');
      }
    }
