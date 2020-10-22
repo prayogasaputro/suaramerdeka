b3a586da0be7bd67df5f158ec5cb1dd647106f6c
prayogasaputro <64127534+prayogasaputro@users.noreply.github.com>
2020-10-22 14:53:20 +0700

    Update cek_bukti.php

--- cek_bukti.php
+++ cek_bukti.php
@@ -1,17 +1,17 @@
 <?php
 session_start();
-if(!isset($_SESSION['username'])) {
+FILTER_INPUT(INPUT_SESSION, 'username') {
    header('location:index.php'); 
 } else { 
-   $username = $_SESSION['username']; 
+   $username = FILTER_INPUT(INPUT_SESSION, 'username'); 
 }
 ?>
 
 <?php
-if( isset($_GET['unik']) ){
+FILTER_INPUT(INPUT_GET, 'unik'){
 
     // ambil id dari query string
-    $unik= $_GET['unik'];
+    $unik= FILTER_INPUT(INPUT_GET, 'unik');
 }
 ?>
 
@@ -20,4 +20,4 @@ include('koneksi.php');
 $aksi  = mysqli_query($db, "select * from image_tb where unik='$unik'");
 $data = mysqli_fetch_array($aksi);
 ?>
-<img src="<?php echo $data['img_location']; ?>" border="0"/>
+<img src="<?php <?= $data['img_location']>?; ?>" border="0"/>
