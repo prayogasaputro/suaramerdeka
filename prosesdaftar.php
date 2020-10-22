6116c8756c27f9abab864f22848467a5025568f7
prayogasaputro <64127534+prayogasaputro@users.noreply.github.com>
2020-10-22 15:16:36 +0700

    Update prosesdaftar.php

--- prosesdaftar.php
+++ prosesdaftar.php
@@ -1,24 +1,24 @@
 <?php
    require_once("koneksi.php");
-   $nama = $_POST['fullName2'];
-   $alamat = $_POST['addressLine12'];
-   $email = $_POST['email2'];
-   $username = $_POST['namaPengguna2'];
-   $pass = $_POST['kataSandi2'];
+   $nama = FILTER_INPUT(INPUT_POST, 'fullName2');
+   $alamat = FILTER_INPUT(INPUT_POST, 'addressLine12');
+   $email = FILTER_INPUT(INPUT_POST, 'email2');
+   $username = FILTER_INPUT(INPUT_POST. 'namaPengguna2');
+   $pass = FILTER_INPUT(INPUT_POST, 'kataSandi2');
    $sql = "SELECT * FROM pemasang WHERE namapengguna2 = '$username'";
    $query = $db->query($sql);
    if($query->num_rows != 0) {
-     echo "<div align='center'>Username Sudah Terdaftar! <a href='index.html'>coba lagi</a></div>";
+     <?= "<div align='center'>Username Sudah Terdaftar! <a href='index.html'>coba lagi</a></div>">?;
    } else {
      if(!$nama || !$alamat || !$email || !$username || !$pass) {
-       echo "<div align='center'>Masih ada data yang kosong! <a href='index.html'>coba lagi</a>";
+       <?= "<div align='center'>Masih ada data yang kosong! <a href='index.html'>coba lagi</a>">?;
      } else {
        $data = "INSERT INTO pemasang VALUES (NULL, '$nama', '$alamat', '$email', '$username', '$pass')";
        $simpan = $db->query($data);
        if($simpan) {
-         echo "<div align='center'>Pendaftaran Sukses, Silahkan <a href='index.html'>Masuk</a></div>";
+         <?= "<div align='center'>Pendaftaran Sukses, Silahkan <a href='index.html'>Masuk</a></div>">?;
        } else {
-         echo "<div align='center'>Proses Gagal!</div>";
+         <?= "<div align='center'>Proses Gagal!</div>">?;
        }
      }
    }
